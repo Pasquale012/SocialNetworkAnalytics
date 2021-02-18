94ec62c9edf639a85cb67c78c222d942f83d4bd2
Pasquale012 <43438431+Pasquale012@users.noreply.github.com>
2021-02-18 19:38:34 +0100

    Modify wsgi.py

--- SocialNetworkAnalyticsCloud/wsgi.py
+++ SocialNetworkAnalyticsCloud/wsgi.py
@@ -13,7 +13,7 @@ from django.core.wsgi import get_wsgi_application
 
 #os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SocialAnalysisCLOUD.settings')
 
-settings_module = "SocialAnalysisCLOUD.production" if 'WEBSITE_HOSTNAME' in os.environ else 'SocialAnalysisCLOUD.settings'
+settings_module = "SocialNetworkAnalyticsCloud.production" if 'WEBSITE_HOSTNAME' in os.environ else 'SocialNetworkAnalyticsCloud.settings'
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
